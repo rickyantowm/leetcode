8a8a3907cf32402af66db8d8daf91bd5172d5ec6
Rickyanto Wangsa Mulya <70902057+rickyantowm@users.noreply.github.com>
2022-12-25 02:54:21 +0700

    Time: 45 ms (82.98%), Space: 15.4 MB (15.99%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        mapS = {}
+        mapT = {}
+        
+        for i in range(len(s)):
+            if s[i] in mapS:
+                mapS[s[i]] = mapS.get(s[i]) + 1
+            else:
+                mapS[s[i]] = 1
+            if t[i] in mapT:
+                mapT[t[i]] = mapT.get(t[i]) + 1
+            else:
+                mapT[t[i]] = 1
+        
+        return mapS == mapT
+    
